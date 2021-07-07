95564b2673fcac00c5f5a906570a2f3d85b7457b
unknown <routhsouravrouth2015@outlook.com>
2021-07-07 20:56:49 +0530

    input fields added

--- component/Login.js
+++ component/Login.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import {View,Text, StyleSheet} from 'react-native';
+import React,{useState} from 'react';
+import {View,Text, StyleSheet,TextInput, Button} from 'react-native';
 
 const Login=(props)=>{
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const handleButton =()=>{
+        alert(name , password)
+    }
     return(
         <View>
             <Text style={styles.font}>{props.data}</Text>
+            <TextInput  style={[styles.input, styles.font]}
+            onChangeText={(e)=> setName(e)}
+            value={name}
+            placeholder="Name"
+            //keyboardType="numeric"
+            />
+            <TextInput  style={[styles.input, styles.font]}
+            onChangeText={(e)=> setPassword(e)}
+            value={password}
+            placeholder="Password"
+            secureTextEntry={true}
+            //keyboardType="numeric"
+            />
+            <Button onPress={handleButton} style={styles.button} title="Login"/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
    font:{
-       fontSize:30,
+       fontSize:20,
        color:"green",
-   }
+   },
+   input:{
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+   },
   });
 
 export default Login;
